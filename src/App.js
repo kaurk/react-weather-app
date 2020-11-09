60355230d2d200ed8e3f875bf67b5afd301b4c2a
kaurk <thind1kamal@gmail.com>
2020-11-09 10:59:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
      Weather APP
+     <footer>
+       This project is coded by Kamaljit Kaur
+     <a href="https://github.com/kaurk/react-weather-app" target="_blank" />
+     and is open sourced on GitHub
+     </footer>
     </div>
   );
 }
