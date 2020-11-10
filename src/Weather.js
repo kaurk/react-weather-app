9cc6cdaea2541e2aa140296d40147acf57479c2c
kaurk <thind1kamal@gmail.com>
2020-11-10 16:44:25 +0100

    Added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -6,10 +6,10 @@ export default function Weather(){
             <form>
                 <div className="row">
                     <div className="col-9">                    
-                <input type="search" placeholder="Type a city..." className="form-control"/>
+                <input type="search" placeholder="Type a city..." className="form-control" autoFocus="on"/>
                 </div>
                 <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary"/>                
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>                
                 </div>
                 </div>
             </form>
