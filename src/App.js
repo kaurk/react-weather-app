c2b1aabef561b25d854c355c36cb349a05afa51a
kaurk <thind1kamal@gmail.com>
2020-11-10 15:12:06 +0100

    Added HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
-
+import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
     <div className="container">
-    Weather APP
+    
+    <Weather/>
      <footer>
        This project is coded by Kamaljit Kaur and is {" "}
      <a href="https://github.com/kaurk/react-weather-app" target="_blank" >
---
+++ src/Weather.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather(){
+    return(
+        <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">                    
+                <input type="search" placeholder="Type a city..." className="form-control"/>
+                </div>
+                <div className="col-3">
+                <input type="submit" value="Search" className="btn btn-primary"/>                
+                </div>
+                </div>
+            </form>
+            <h1>Zürich</h1>
+            <ul>
+                <li>
+                    Tuesday,14:36
+                </li>
+                <li>
+                    Partly Cloudy
+                </li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="Partly Cloudy" />
+                    10 °C
+                </div>
+                <div className="row-6">
+                    <ul>
+                        <li>
+                            Precipitation:20%
+                        </li>
+                        <li>
+                            Humidity:70%
+                            </li>
+                        <li>
+                            Wind:10 km/h
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
