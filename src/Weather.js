8802062faed204d259960deb29bf570caccec659
kaurk <thind1kamal@gmail.com>
2020-11-12 11:42:38 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,7 @@ export default function Weather(props){
 const[weatherData, setWeatherData] = useState({ready:false});
 
 function handleResponse(response){
-console.log(response.data);
+//console.log(response.data);
 
 setWeatherData({
     ready:true,
